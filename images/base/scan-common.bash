8544c42f81efa68f9556240b0f084645a4de1a6d
Timo Pagel <github@timo-pagel.de>
2021-08-27 11:30:36 +0200

    feat: add cachedUsedAt to JSON

--- images/base/scan-common.bash
+++ images/base/scan-common.bash
@@ -1,23 +1,32 @@
 #!/bin/bash
 
 function scan_result_post {
-  JSON_RESULT=$(echo "${JSON_RESULT}" | jq -Sc ". += {\"finishedAt\": \"$(date --rfc-3339=ns)\"}")
+  if [ ${IS_USE_CACHE} ]; then
+    JSON_RESULT=$(echo "${JSON_RESULT}" | jq -Sc ". += {\"cachedUsedAt\": \"$(date --rfc-3339=ns)\"}")
+  else
+    JSON_RESULT=$(echo "${JSON_RESULT}" | jq -Sc ". += {\"finishedAt\": \"$(date --rfc-3339=ns)\"}")
+  fi
+
   cat <<<"$(echo "{}" | jq ".\"${MODULE_NAME}\" = $(echo "${JSON_RESULT}")")" > "${ARTIFACTS_PATH}/module_${MODULE_NAME}.json"
   echo "${JSON_RESULT}"
 }
 
+IS_USE_CACHE=false
 function scan_result_pre {
   if [ "${RESULT_CACHING_HOURS}" == "" ]; then RESULT_CACHING_HOURS=4; fi
   RESULT_CACHING_MIN=$( expr ${RESULT_CACHING_HOURS} \* 60)
-  JSON_RESULT=$(echo "{}" | jq -Sc ".+= {\"startedAt\": \"$(date --rfc-3339=ns)\"}")
-  echo "mkdir ${ARTIFACTS_PATH}"
-  mkdir -p "${ARTIFACTS_PATH}" || true
+
   RESULT_FILE="${ARTIFACTS_PATH}/module_${MODULE_NAME}.json"
   if [ -f "${RESULT_FILE}" ] && [[ $(find "${RESULT_FILE}" -mmin -${RESULT_CACHING_MIN} -print) ]]; then
     echo "Scan has been performed, already, using old result"
+    IS_USE_CACHE=true
     JSON_RESULT=$(cat "${RESULT_FILE}" | jq ".\"${MODULE_NAME}\"")
     scan_result_post
     exit 0
   fi
+
+  JSON_RESULT=$(echo "{}" | jq -Sc ".+= {\"startedAt\": \"$(date --rfc-3339=ns)\"}")
+  echo "mkdir ${ARTIFACTS_PATH}"
+  mkdir -p "${ARTIFACTS_PATH}" || true
 }
 
