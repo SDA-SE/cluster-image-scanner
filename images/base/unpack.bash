cebe26d9d0e30a2869e0caa603ecba5b18e03d6e
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-06 13:59:01 +0200

    feat: Move IMAGE_UNPACKED_DIRECTORY

--- images/base/unpack.bash
+++ images/base/unpack.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-mkdir -p "${IMAGE_UNPACKED_DIRECTORY}" || true
-cd "${IMAGE_UNPACKED_DIRECTORY}"
-tar xf "${IMAGE_TAR_PATH}"
-cd /clusterscanner
+if [ -e "${IMAGE_UNPACKED_DIRECTORY}" ]; then
+  echo "IMAGE_UNPACKED_DIRECTORY ${IMAGE_UNPACKED_DIRECTORY} exists"
+else
+  echo "Unpacking ${IMAGE_TAR_PATH} to ${IMAGE_UNPACKED_DIRECTORY}"
+  mkdir -p "${IMAGE_UNPACKED_DIRECTORY}" || true
+  cd "${IMAGE_UNPACKED_DIRECTORY}"
+  tar xf "${IMAGE_TAR_PATH}"
+  cd -
+fi
