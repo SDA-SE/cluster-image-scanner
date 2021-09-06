1dbcc8bfdc47b9ca6f7057020cb55e2f1e92b927
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-06 16:07:01 +0200

    chore: Enhance debug

--- images/base/unpack.bash
+++ images/base/unpack.bash
@@ -4,7 +4,7 @@ set -e
 if [ -d "${IMAGE_UNPACKED_DIRECTORY}" ]; then
   echo "IMAGE_UNPACKED_DIRECTORY ${IMAGE_UNPACKED_DIRECTORY} exists"
 else
-  echo "ERROR: Please delete the complete image PVC, we try to unpack into it directly"
+  echo "ERROR: Please delete the complete image PVC, we try to unpack into within the image-fetcher (${IMAGE_UNPACKED_DIRECTORY})"
   #echo "Unpacking ${IMAGE_TAR_PATH} to ${IMAGE_UNPACKED_DIRECTORY}"
   #mkdir -p "${IMAGE_UNPACKED_DIRECTORY}" || true
   #cd "${IMAGE_UNPACKED_DIRECTORY}"
