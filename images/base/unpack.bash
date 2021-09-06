ff3e3809b048055827247bf0ffbd63e40f1456fe
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-06 14:30:01 +0200

    chore: add warning by using old scanjob

--- images/base/unpack.bash
+++ images/base/unpack.bash
@@ -4,9 +4,10 @@ set -e
 if [ -d "${IMAGE_UNPACKED_DIRECTORY}" ]; then
   echo "IMAGE_UNPACKED_DIRECTORY ${IMAGE_UNPACKED_DIRECTORY} exists"
 else
-  echo "Unpacking ${IMAGE_TAR_PATH} to ${IMAGE_UNPACKED_DIRECTORY}"
-  mkdir -p "${IMAGE_UNPACKED_DIRECTORY}" || true
-  cd "${IMAGE_UNPACKED_DIRECTORY}"
-  tar xf "${IMAGE_TAR_PATH}"
-  cd -
+  echo "ERROR: Please delete the complete image PVC, we try to unpack into it directly"
+  #echo "Unpacking ${IMAGE_TAR_PATH} to ${IMAGE_UNPACKED_DIRECTORY}"
+  #mkdir -p "${IMAGE_UNPACKED_DIRECTORY}" || true
+  #cd "${IMAGE_UNPACKED_DIRECTORY}"
+  #tar xf "${IMAGE_TAR_PATH}"
+  #cd -
 fi
