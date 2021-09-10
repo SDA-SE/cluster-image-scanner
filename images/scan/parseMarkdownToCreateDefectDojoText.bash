ebda09f8d1ab55f604ecc9d2133cd721a829b7f5
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-10 11:24:28 +0200

    feat: read template from docs

--- images/scan/parseMarkdownToCreateDefectDojoText.bash
+++ images/scan/parseMarkdownToCreateDefectDojoText.bash
@@ -22,15 +22,10 @@ sed -i "s/##/#/g" ${TEMP_FILE}
 sed -i "s/# //g" ${TEMP_FILE}
 sed -i "s/#/\n/g" ${TEMP_FILE}
 extract=$(cat ${TEMP_FILE} | sed 's#|#_#g' | tr '\n' "|")
-#sed -e "s|###${TARGET}###|${extract}|" ${TARGET_FILE}
 
 sed -i -e "s/###${TARGET}###/${extract}/g"  ${TARGET_FILE}
 sed -i -e "s/|/\n/g"  ${TARGET_FILE}
-echo "####################"
-#cat ${TARGET_FILE}
 
-#awk "/###${TARGET}###/{system(\"cat ${TEMP_FILE}\")}1" ${TARGET_FILE} > ${TEMP_FILE}2
-#mv ${TEMP_FILE}2 ${TARGET_FILE}
 
 
 
