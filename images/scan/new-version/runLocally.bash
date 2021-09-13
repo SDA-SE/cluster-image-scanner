9f3294806c2024f724930721f7faf001c7cd010e
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-13 15:21:02 +0200

    feat: add versions with v

--- images/scan/new-version/runLocally.bash
+++ images/scan/new-version/runLocally.bash
@@ -3,7 +3,8 @@
 export IMAGE=quay.io/sdase/cluster-image-scanner-imagecollector:2.0.339 # last Version
 export IMAGE=quay.io/sdase/cluster-image-scanner-imagecollector:1.9.9
 export IMAGE=quay.io/sdase/cluster-image-scanner-imagecollector:2.0.338
-export IMAGE_SCAN_POSITIVE_FILTER="quay.io/sdase/"
+export IMAGE=dexidp/dex:v2.29.0
+export IMAGE_SCAN_POSITIVE_FILTER="quay.io/sdase/|dexidp/dex"
 source env.bash
 cp ../../base/scan-common.bash .
 
