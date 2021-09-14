40d4ddff0cd0c714236dceb2a0babb02fd26e18e
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-14 09:13:13 +0200

    feat: use image-unpacked only

--- images/scan/distroless/module.bash
+++ images/scan/distroless/module.bash
@@ -5,7 +5,6 @@ set -e
 # by looking for a shell
 #
 # Usage example
-# export IMAGE_TAR_PATH=/path/to/image.tar"
 # export ARTIFACTS_PATH=/path/to/output/dir"
 # ./module.bash
 
@@ -14,12 +13,12 @@ source /clusterscanner/scan-common.bash
 scan_result_pre
 
 _shell_found=0
-
-for sh in "bin/sh$" "bin/bash$" "bin/dash$" "bin/zsh$" "bin/ash$"; do
-    if tar -tf "${IMAGE_TAR_PATH}" | grep -q ${sh}; then
-        _sh_name=$(echo "${sh}" | rev | cut -c2- | rev)
-        echo "${_sh_name} found"
-        JSON_RESULT=$(echo "${JSON_RESULT}" | jq -Sc ".shells += [\"${_sh_name}\"]")
+potentialBashs=("bin/sh" "bin/bash" "bin/dash" "bin/zsh" "bin/ash")
+for sh in ${potentialBashs[@]}; do
+    if [[ -e "${IMAGE_UNPACKED_DIRECTORY}/${sh}" ]]; then
+      echo $sh
+        echo "${sh} found"
+        JSON_RESULT=$(echo "${JSON_RESULT}" | jq -Sc ".shells += [\"${sh}\"]")
         _shell_found=1
     fi
 done
