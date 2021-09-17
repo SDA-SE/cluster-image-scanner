3dd8bbff1c7597d661f784ef473e9562f03c9167
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-17 09:27:01 +0200

    chore: add debug

--- images/base/scan-common.bash
+++ images/base/scan-common.bash
@@ -20,6 +20,7 @@ function scan_result_pre {
   if [ -f "${RESULT_FILE}" ]; then
     lastStatus=$(cat "${RESULT_FILE}" | jq -r ".${MODULE_NAME} | .status" 2>/dev/null || true)
     if [ "${lastStatus}" != "completed" ] && [ "${IS_SCAN}" == "true" ]; then
+      echo "lastStatus: ${lastStatus}"
       echo "Removing ${RESULT_FILE} because this scan changed from skip=true to skip=false"
       rm "${RESULT_FILE}"
     fi
