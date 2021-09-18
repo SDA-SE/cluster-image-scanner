8f466898850631d91e5e52bd726fafec6ea1f7da
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-18 14:26:07 +0200

    fix: lastStatus

--- images/base/scan-common.bash
+++ images/base/scan-common.bash
@@ -19,7 +19,7 @@ function scan_result_pre {
   RESULT_FILE="${ARTIFACTS_PATH}/module_${MODULE_NAME}.json"
   if [ -f "${RESULT_FILE}" ]; then
     cat "${RESULT_FILE}"
-    lastStatus=$(cat "${RESULT_FILE}" | jq -r ".${MODULE_NAME} | .status" 2>/dev/null || true)
+    lastStatus=$(cat "${RESULT_FILE}" | jq -r '.["'${MODULE_NAME}'"] | .status' 2>/dev/null || true)
     if [ "${lastStatus}" != "completed" ] && [ "${IS_SCAN}" == "true" ]; then
       echo "lastStatus: ${lastStatus}"
       echo "Removing ${RESULT_FILE} because this scan changed from skip=true to skip=false"
