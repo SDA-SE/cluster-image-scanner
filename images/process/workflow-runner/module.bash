d1f02fde4dee5a259ecf7b1f92b39ea4bbf2dc73
Timo Pagel <timo.pagel.extern@signal-iduna.de>
2021-09-21 10:06:36 +0200

    feat: add custom imageNames

--- images/process/workflow-runner/module.bash
+++ images/process/workflow-runner/module.bash
@@ -38,8 +38,14 @@ while read -r line; do
   sed -i "s~###is_scan_new_version###~${IS_SCAN_NEW_VERSION}~" /clusterscanner/template.yml
   sed -i "s~###scan_lifetime_max_days###~$(echo "${DATA_JSON}" | jq -r .scan_lifetime_max_days)~" /clusterscanner/template.yml
   sed -i "s~###new_version_image_filter###~${NEW_VERSION_IMAGE_FIILTER}~" /clusterscanner/template.yml
-
-
+  sed -i "s~###baseImageName###~${baseImageName}~" /clusterscanner/template.yml
+  sed -i "s~###defectDojoClientImageName###~${defectDojoClientImageName}~" /clusterscanner/template.yml
+  sed -i "s~###scanDistrolessImageName###~${scanDistrolessImageName}~" /clusterscanner/template.yml
+  sed -i "s~###scanDependencyCheckImageName###~${scanDependencyCheckImageName}~" /clusterscanner/template.yml
+  sed -i "s~###scanMalwareImageName###~${scanMalwareImageName}~" /clusterscanner/template.yml
+  sed -i "s~###scanRootImageName###~${scanRootImageName}~" /clusterscanner/template.yml
+  sed -i "s~###scanLifetimeImageName###~${scanLifetimeImageName}~" /clusterscanner/template.yml
+  sed -i "s~###scanNewVersionImageName###~${scanNewVersionImageName}~" /clusterscanner/template.yml
 
   cat /clusterscanner/template.yml
   kubectl create -n clusterscanner -f /clusterscanner/template.yml
