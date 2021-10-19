7f8d662ec84e7f0efc8d1770465948c8aa573af9
Timo Pagel <git@timo-pagel.de>
2021-10-19 09:06:29 +0200

    chore: fix name

--- images/process/workflow-runner/env.bash
+++ images/process/workflow-runner/env.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export MODULE_NAME="workflow-runner"
-export TITLE="CllusterScanner Workflow Runner"
+export TITLE="ClusterImageScanner Workflow Runner"
 export DESCRIPTION="Starts workflows to scan images"
