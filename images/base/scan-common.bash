35f8e6a90507d189a2c98ed6efad26e48197c81b
Timo Pagel <github-commit@timo-pagel.de>
2022-03-09 18:41:16 +0100

    chore: add debugging

--- images/base/scan-common.bash
+++ images/base/scan-common.bash
@@ -28,6 +28,7 @@ function scan_result_pre {
       fi
       if [[ $(find "${RESULT_FILE}" -mmin -${RESULT_CACHING_MIN} -print 2>/dev/null) ]]; then
         echo "Scan has been performed, already, using old result (RESULT_CACHING_MIN ${RESULT_CACHING_MIN}), ARTIFACTS_PATH ${ARTIFACTS_PATH}:"
+        rm ${ARTIFACTS_PATH}/dependency-check* || true
         cat  ${ARTIFACTS_PATH}/dependency-check-report.xml || true
         ls -la "${ARTIFACTS_PATH}"
         IS_USE_CACHE=true
