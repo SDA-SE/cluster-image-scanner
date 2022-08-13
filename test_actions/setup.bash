b47499e964028347b44bae8c4403ed57ea912fd4
Timo Pagel <github@timo-pagel.de>
2022-08-13 17:41:53 +0200

    add env vars and include syft in needs

--- test_actions/setup.bash
+++ test_actions/setup.bash
@@ -141,13 +141,12 @@ sleep 5
 argo list workflows -A
 workflow=$(argo -n clusterscanner list | grep orchestration | awk '{print $1}')
 echo "will wait for workflow ${workflow}"
-
-pod=$(argo get ${workflow} -n clusterscanner | grep orch | awk '{print $4}' | grep orch)
+#pod=$(argo get ${workflow} -n clusterscanner | grep orch | awk '{print $4}' | grep orch)
 
 until [[ $(argo list -A | grep ${workflow} | grep Running | wc -l) -ne "1" ]]
 do
   for i in $(argo list -A | awk '{print $2}'| grep -v "^NAME"); do argo get --no-utf8 $i -n clusterscanner;done
-  kubectl describe pod   $pod -n clusterscanner
+  #kubectl describe pod $pod -n clusterscanner
   sleep 30;
 done
 rm -Rf ./tmp || true
