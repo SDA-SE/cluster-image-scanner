f7d24b292e8fe23639b6ccbb6d8d3c8e21fac719
Timo Pagel <github@timo-pagel.de>
2022-08-15 16:08:20 +0200

    add test image

---
+++ images/process/test-image/env.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export MODULE_NAME="test-image"
+export TITLE="ClusterScanner Test Image"
+export DESCRIPTION="Provides an Images with vulnerabilities and misconfigurations"
---
+++ images/process/test-image/module.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+echo "Nothing to do, this image is not there to be executed."
