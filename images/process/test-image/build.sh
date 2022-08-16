7e857fb63faa67ef7aaf37d164207ac6d6b58d6f
Timo Pagel <github@timo-pagel.de>
2022-08-16 07:36:05 +0200

    Testing merge (#63)
    * add tests
    
    * add tests
    
    * add test-job
    
    * re-add syft to build
    
    * fix SLACK token check
    
    * re-add dependency-track and enable job
    
    * change secret to placeholder in env vars
    
    * change secret to placeholder in env vars
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * debug
    
    * debug
    
    * Update setup.bash
    
    * Update main.yml
    
    * Update setup.bash
    
    * Update setup.bash
    
    * Update setup.bash
    
    * Update setup.bash
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update setup.bash
    
    * Update main.yml
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * add env vars and include syft in needs
    
    * enhance debugging
    
    * add concurrency
    
    * add debugging
    
    * Update setup.bash
    
    * Update setup.bash
    
    * Update main.yml
    
    * enhance debugging
    
    * enhance debugging
    
    * enhance debugging
    
    * change URL
    
    * change DD-Parameter handling
    
    * add test DD
    
    * adjust RAM sbom
    
    * fix DD_parameters
    
    * enhance readme
    
    * reduce debugging
    
    * reduce debugging
    
    * reduce debugging
    
    * reduce debugging
    
    * reduce debugging
    
    * reduce debugging
    
    * reduce debugging
    
    * add test image
    
    * add test image
    
    * add test image
    
    * add test image
    
    * add test image
    
    * add test image
    
    * add test image
    
    * add test image
    
    * ch

---
+++ images/process/test-image/build.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+set -e
+source ./env.bash
+if [ $# -ne 7 ]; then
+  echo "Parameters are not set correctly"
+  exit 1
+fi
+
+REGISTRY=$1
+ORGANIZATION=$2
+IMAGE_NAME=$3
+VERSION=$4
+REGISTRY_USER=$5
+REGISTRY_TOKEN=$6
+BUILD_EXPORT_OCI_ARCHIVES=$7
+
+MAJOR=$(echo "${VERSION}" | tr  '.' "\n" | sed -n 1p)
+MINOR=$(echo "${VERSION}" | tr  '.' "\n" | sed -n 2p)
+
+oci_prefix="org.opencontainers.image"
+
+trap cleanup INT EXIT
+cleanup() {
+  test -n "${ctr}" && buildah rm "${ctr}" || true
+}
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+build_dir="${dir}/build"
+
+
+base_image="registry.access.redhat.com/ubi8/ubi-init" # minimal doesn't have useradd
+ctr_tools="$(buildah from --pull --quiet ${base_image})"
+
+base_image="quay.io/sdase/cluster-image-scanner-base:2"
+ctr="$( buildah from --pull --quiet "${base_image}")"
+mnt="$( buildah mount "${ctr}" )"
+
+dnf_opts=(
+  "--installroot=/mnt"
+  "--assumeyes"
+  "--setopt=install_weak_deps=false"
+  "--releasever=8"
+  "--setopt=tsflags=nocontexts,nodocs"
+  "--quiet"
+)
+
+buildah run --volume "${mnt}:/mnt" "${ctr_tools}" -- /usr/bin/dnf install -y "${dnf_opts[@]}" curl git openssl openssh
+buildah run --volume "${mnt}:/mnt" "${ctr_tools}" -- /usr/bin/dnf clean "${dnf_opts[@]}" all
+rm -rf "${mnt}"/var/{cache,log}/* "${mnt}"/tmp/*
+mkdir "${mnt}/vulnerable-files/"
+cp log4j-core-2.14.0.jar "${mnt}/vulnerable-files/log4j-core-2.14.0.jar"
+echo 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*' > "${mnt}/vulnerable-files/eicar.txt"
+find $mnt
+
+# Get a bill of materials
+base_bill_of_materials_hash=$(buildah inspect --type image "${base_image}"  | jq '.OCIv1.config.Labels."io.sda-se.image.bill-of-materials-hash"')
+#echo "base_bill_of_materials_hash $base_bill_of_materials_hash"
+bill_of_materials_hash="$( ( cat "${0}";
+  echo "${base_bill_of_materials_hash}"; \
+  find ${mnt}; \
+  cat ./*; \
+  ) | sha256sum | awk '{ print $1 }' )"
+echo "bill_of_materials: $bill_of_materials_hash";
+buildah config \
+  --label "${oci_prefix}.url=https://quay.io/sdase/cluster-image-scanner-${MODULE_NAME}" \
+  --label "${oci_prefix}.source=https://github.com/SDA-SE/clusterscanner-${MODULE_NAME}" \
+  --label "${oci_prefix}.revision=$( git rev-parse HEAD )" \
+  --label "${oci_prefix}.version=${VERSION}" \
+  --label "${oci_prefix}.title=${TITLE}" \
+  --label "${oci_prefix}.description=${DESCRIPTION}" \
+  --label "io.sda-se.image.bill-of-materials-hash=${bill_of_materials_hash}" \
+  --user 0 \
+  "${ctr}"
+
+buildah commit --timestamp "1655294536" --quiet "${ctr}" "${IMAGE_NAME}:${VERSION}" && ctr=
+
+if [ -n "${BUILD_EXPORT_OCI_ARCHIVES}" ]
+then
+  mkdir --parent "${build_dir}"
+  image="docker://${REGISTRY}/${ORGANIZATION}/${IMAGE_NAME}:${VERSION}"
+  buildah push --quiet --creds "${REGISTRY_USER}:${REGISTRY_TOKEN}" "${IMAGE_NAME}:${VERSION}" "${image}"
+
+  image="docker://${REGISTRY}/${ORGANIZATION}/${IMAGE_NAME}:${MAJOR}.${MINOR}"
+  buildah push --quiet --creds "${REGISTRY_USER}:${REGISTRY_TOKEN}" "${IMAGE_NAME}:${VERSION}" "${image}"
+
+  image="docker://${REGISTRY}/${ORGANIZATION}/${IMAGE_NAME}:${MAJOR}"
+  buildah push --quiet --creds "${REGISTRY_USER}:${REGISTRY_TOKEN}" "${IMAGE_NAME}:${VERSION}" "${image}"
+
+  buildah rmi "${IMAGE_NAME}:${VERSION}"
+fi
+
+cleanup
---
+++ test_actions/setup.bash
@@ -0,0 +1,170 @@
+#!/bin/bash
+set -e
+# shellcheck disable=SC2026,SC2154
+for pid in $(ps -ef | grep port-forward | grep "svc/argo-server\|svc/minio-hl"  | awk '{print $2}');do kill $pid;done
+
+if [ "${IS_MINIKUBE}" == "true" ]; then
+  minikube delete
+  minikube start --memory=29384 --cpus=8 --vm-driver kvm2 --disk-size 150GB
+  echo "Maybe you want to run 'minikube addons configure registry-creds' with registry 'https://index.docker.io/v1/', press any key to continue"
+  read -n 1 -s
+fi
+
+if [ "${SECRETS_PATH}" != "" ]; then
+  source ${SECRETS_PATH}
+elif [ "${DD_TOKEN_SECRET}" == "" ]; then
+  echo "Error, SECRETS_PATH doesn't exists and env variables not set";
+  exit 1;
+fi
+
+wait_for_pods_ready () {
+  local name="${1}"; shift
+  local namespace="${1}"; shift
+  local count="${1}"; shift
+  local sleep="${1}"; shift
+  local max_attempts="${1}"
+  local attempt_num=0
+
+  until [[ $(kubectl -n "${namespace}" get pods -o json | jq '.items | length') -ge "${count}" ]]
+  do
+    if [[ $(( attempt_num++ )) -ge "${max_attempts}" ]]
+    then
+      echo "max_attempts ${max_attempts} reached, aborting"
+      kubectl get pods -A
+      exit 1
+    fi
+    echo "waiting for ${name} to be created"
+    sleep "${sleep}"
+  done
+  until [[ $(kubectl -n "${namespace}" get pods -o json | jq '.items[].status.conditions[].status=="True"' | grep -c false) -eq "0" ]]
+  do
+    if [[ $(( attempt_num++ )) -ge "${max_attempts}" ]]
+    then
+      echo "max_attempts ${max_attempts} reached, aborting"
+      kubectl get pods -A
+      exit 1
+    fi
+    echo "waiting for ${name} to be up"
+    sleep "${sleep}"
+  done
+}
+
+DEPLOYMENT_PATH=../deployment
+sed -i "s#ACCESS_KEY#testtesttest#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/s3.env
+sed -i "s#SECRET_KEY#testtesttest#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/s3.env
+
+sed -i "s#DD_TOKEN_SECRET#${DD_TOKEN_SECRET}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/defectdojo.secret.env
+sed -i "s#DD_URL_PLACEHOLDER#${DD_URL_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/defectdojo.cm.env
+sed -i "s#DD_USER_PLACEHOLDER#${DD_USER_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/defectdojo.cm.env
+sed -i "s#DD_TEST_TOKEN_SECRET#${DD_TEST_TOKEN_SECRET}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/defectdojo-test.secret.env
+sed -i "s#DD_TEST_URL_PLACEHOLDER#${DD_TEST_URL_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/defectdojo-test.cm.env
+sed -i "s#DD_TEST_USER_PLACEHOLDER#${DD_TEST_USER_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/defectdojo.cm.env
+
+sed -i "s#SLACK_CLI_TOKEN_SECRET#${SLACK_CLI_TOKEN_SECRET}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/slack.env
+
+sed -i "s#GITHUB_APP_ID_PLACEHOLDER#${GH_APP_ID}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github.env
+sed -i "s#GITHUB_APP_LOGIN_PLACEHOLDER#${GITHUB_APP_LOGIN_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github.env
+sed -i "s#GITHUB_INSTALLATION_ID_PLACEHOLDER#${GH_INSTALLATION_ID}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github.env
+echo "${GH_PRIVATE_KEY}" > ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github_private_key.pem
+
+sed -i "s#DEPSCAN_DB_DRIVER_PLACEHOLDER#${DEPSCAN_DB_DRIVER_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/depcheck.env
+sed -i "s#DEPSCAN_DB_USERNAME_PLACEHOLDER#${DEPSCAN_DB_USERNAME_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/depcheck.env
+sed -i "s#DEPSCAN_DB_PASSWORD_PLACEHOLDER#${DEPSCAN_DB_PASSWORD_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/depcheck.env
+sed -i "s#DEPSCAN_DB_CONNECTSRING_PLACEHOLDER#${DEPSCAN_DB_CONNECTSRING_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/depcheck.env
+
+sed -i "s#smtp_SECRET#${smtp_SECRET}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/email.env
+sed -i "s#smtp_auth_SECRET#${smtp_auth_SECRET}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/email.env
+sed -i "s#smtp_auth-user_SECRET#${smtp_auth_user_SECRET}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/email.env
+sed -i "s#smtp_auth-password_SECRET#${smtp_auth_password_SECRET}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/email.env
+
+sed -i "s#DEPENDENCY_TRACK_URL_PLACEHOLDER#${DEPENDENCY_TRACK_URL_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/dependency-track.cm.env
+sed -i "s#DEPENDENCY_TRACK_KEY_PLACEHOLDER#${DEPENDENCY_TRACK_KEY_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/dependency-track.secret.env
+
+kubectl apply -k argocd
+wait_for_pods_ready "argocd" "argocd" 5 10 120
+
+kubectl apply -f argocd.project.yml
+
+# kustomize is not supported
+mkdir tmp || true
+kubectl apply -k argowf
+curl -sL --output ./tmp/namespace-install.yaml "https://github.com/argoproj/argo-workflows/releases/download/v3.3.8/namespace-install.yaml"
+kubectl apply -f ./tmp/namespace-install.yaml -n clusterscanner
+
+
+kubectl apply -k minio
+
+wait_for_pods_ready "minio" "minio-operator" 2 10 120
+wait_for_pods_ready "argo-workflow" "clusterscanner" 2 10 120
+
+kubectl kustomize --load-restrictor LoadRestrictionsNone base > tmp.yml
+kubectl apply -f tmp.yml
+rm tmp.yml
+
+if ! which mc > /dev/null 2>&1; then
+  curl -sL https://dl.min.io/client/mc/release/linux-amd64/mc --output ./tmp/mc
+  chmod +x ./tmp/mc
+  PATH=$PATH:./tmp
+fi
+
+if ! which argo > /dev/null 2>&1; then
+  curl -sLO https://github.com/argoproj/argo-workflows/releases/download/v3.3.8/argo-linux-amd64.gz
+  gunzip argo-linux-amd64.gz
+  chmod +x argo-linux-amd64
+  mv ./argo-linux-amd64 ./tmp/argo
+  argo=./tmp/argo
+  PATH=$PATH:./tmp
+fi
+
+sleep 30
+
+wait_for_pods_ready "minio tenant" "clusterscanner" 3 10 120
+
+sleep 10
+echo "adding port-forward"
+kubectl -n clusterscanner port-forward svc/argo-server 2746:2746 &
+kubectl -n clusterscanner port-forward svc/minio-hl 9000:9000 &
+
+sleep 2
+
+mc alias set local http://127.0.0.1:9000 testtesttest testtesttest || true
+mc mb local/local || true
+
+echo "submitting argo-main.yml"
+argo submit -n clusterscanner ../argo-main.yml
+
+if [ "${IS_MINIKUBE}" == "true" ]; then
+  echo "Token:"
+  server=$(kubectl get pods -n clusterscanner | grep argo-server | awk '{print $1}'); kubectl -n clusterscanner exec pod/$server -- argo auth token
+  echo "server=\$(kubectl get pods -n clusterscanner | grep argo-server | awk '{print \$1}'); kubectl -n clusterscanner exec pod/\$server -- argo auth token"
+  echo "${server}"
+fi
+
+sleep 5
+argo list workflows -A
+workflow=$(argo -n clusterscanner list | grep orchestration | awk '{print $1}')
+echo "will wait for workflow ${workflow}"
+
+until [[ $(argo list -A | grep ${workflow} | grep Running | wc -l) -ne "1" ]]
+do
+  for i in $(argo list -A | awk '{print $2}'| grep -v "^NAME"); do 
+    argo get --no-utf8 $i -n clusterscanner;
+    echo "######################################################################################################## argo get"
+  done
+#  for pod in $(kubectl get pod -n clusterscanner | grep -v ContainerCreating  | grep -v Pending | grep -v Completed | grep -v NAME | awk '{print $1}'); do
+#      echo "######################################################################################################## pod logs $pod"
+#      kubectl logs ${pod} -n clusterscanner || true
+#  done
+  sleep 60;
+done
+if [ $(argo list workflows -A | grep -c -i "Error\|Failed") -ne 0 ]; then
+  echo "ERRORs during workflow execution"
+  for pod in $(kubectl get pod -n clusterscanner | grep "Error\|Failed" | awk '{print $1}'); do
+      echo "######################################################################################################## pod logs $pod"
+      kubectl logs ${pod} -n clusterscanner || true
+  done
+  exit 1
+fi
+argo list workflows -A
+rm -Rf ./tmp || true
+
