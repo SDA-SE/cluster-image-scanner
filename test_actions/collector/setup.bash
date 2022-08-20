123f8581930c9f818f1e36bef7e4085f6f295887
Timo Pagel <github@timo-pagel.de>
2022-08-20 15:34:05 +0200

    install sleep

--- test_actions/collector/setup.bash
+++ test_actions/collector/setup.bash
@@ -7,21 +7,22 @@ source ../library.bash
 
 sed -i "s~###VERSION###~${VERSION}~g" application/deployment.yaml
 sed -i "s~###VERSION###~${VERSION}~g" job.yml
-sed -i "s~###GITHUB_TARGET_REPOSITORY###~${GITHUB_TARGET_REPOSITORY}~g" job.yml
+sed -i "s~###GIT_TARGET_REPOSITORY###~${GIT_TARGET_REPOSITORY}~g" job.yml
 
 kubectl apply -k ./application
 wait_for_pods_ready "test deployment of image" "shire" 1 10 120
-
-
-
 
 sed -i "s#GITHUB_APP_ID_PLACEHOLDER#$GITHUB_APP_ID_PLACEHOLDER#" configmap.yaml
 sed -i "s#GITHUB_APP_LOGIN_PLACEHOLDER#$GITHUB_APP_LOGIN_PLACEHOLDER#" configmap.yaml
 sed -i "s#GITHUB_INSTALLATION_ID_PLACEHOLDER#$GITHUB_INSTALLATION_ID_PLACEHOLDER#" configmap.yaml
 
 kubectl apply -k .
+if [ -e "${GH_PRIVATE_KEY_PATH}" ]; then
+  kubectl create secret generic github --from-file="keyfile=${GH_PRIVATE_KEY_PATH}" -n cluster-image-scanner-image-collector
+else
+  kubectl create secret generic github --from-literal="keyfile=not-existing" -n cluster-image-scanner-image-collector
+fi
 
-kubectl create secret generic github --from-file="keyfile=${GH_PRIVATE_KEY_PATH}" -n cluster-image-scanner-image-collector
 
 wait_for_pods_completed "collector" "cluster-image-scanner-image-collector" 1 10 120
 
