4b642509b2c09e984315ce3d712d41c0df5b8212
Timo Pagel <github@timo-pagel.de>
2022-08-20 12:29:46 +0200

    install sleep

--- test_actions/library.bash
+++ test_actions/library.bash
@@ -31,6 +31,37 @@ wait_for_pods_ready () {
     sleep "${sleep}"
   done
 }
+wait_for_pods_completed () {
+  local name="${1}"; shift
+  local namespace="${1}"; shift
+  local count="${1}"; shift
+  local sleep="${1}"; shift
+  local max_attempts="${1}"
+  local attempt_num=0
+
+  until [[ $(kubectl get pods -n ${namespace} | grep -c Running) -eq ${count} ]]
+  do
+    if [[ $(( attempt_num++ )) -ge "${max_attempts}" ]]
+    then
+      echo "max_attempts ${max_attempts} reached, aborting"
+      kubectl get pods -A
+      exit 1
+    fi
+    echo "waiting for ${name} to be created"
+    sleep "${sleep}"
+  done
+  until [[ $(kubectl get pods -n ${namespace} | grep -c Running) -eq 0 ]]
+  do
+    if [[ $(( attempt_num++ )) -ge "${max_attempts}" ]]
+    then
+      echo "max_attempts ${max_attempts} reached, aborting"
+      kubectl get pods -A
+      exit 1
+    fi
+    echo "waiting for ${name} to be done"
+    sleep "${sleep}"
+  done
+}
 
 export BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export MAJOR="2"
@@ -41,7 +72,7 @@ if [ "${BRANCH}" != "master" ] && [ "${BRANCH}" != "head" ]; then
   export MAJOR=$(echo ${BRANCH} | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9._-]//g')
   export PATCH=""
   if [ "${GITHUB_RUN_NUMBER}" != "" ]; then
-    export MAJOR=$(echo ${GITHUB_REF##*/} | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9._-]//g')
+    export MAJOR=$(echo ${GITHUB_BASE_REF##*/} | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9._-]//g')
     export MINOR=".${GITHUB_RUN_NUMBER}"
   else
     export MINOR=""
