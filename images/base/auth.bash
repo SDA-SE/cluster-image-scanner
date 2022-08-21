96ee924c3c44c695581450628f605b94c85c0068
Timo Pagel <github@timo-pagel.de>
2022-08-21 16:58:48 +0200

    install sleep

--- images/base/auth.bash
+++ images/base/auth.bash
@@ -25,8 +25,7 @@ EOF
     payload_base64=$(echo -n "${payload}" | jq -c . | base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
 
     header_payload=$(echo -n "${header_base64}.${payload_base64}")
-    echo "Creating Signature with header_payload $header_payload AND GH_KEY_FILE_PATH: $GH_KEY_FILE_PATH"
-    ls -la $GH_KEY_FILE_PATH
+    echo "Creating Signature with header_payload ${header_payload} AND GH_KEY_FILE_PATH: $GH_KEY_FILE_PATH"
     signature=$(echo -n "${header_payload}" | openssl dgst -binary -sha256 -sign "${GH_KEY_FILE_PATH}" | base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
 
     export CLUSTER_SCAN_JWT="${header_payload}.${signature}"
@@ -38,16 +37,15 @@ sp_authorize() {
       return 1
     fi
     createJWT
-    createdGithubToken=true
     echo "Creating GH_TOKEN"
-    GH_TOKEN=$(curl -X POST -H "Authorization: Bearer ${CLUSTER_SCAN_JWT}" -H "Accept: application/vnd.github.machine-man-preview+json" https://api.github.com/app/installations/"${GH_INSTALLATION_ID}"/access_tokens | jq '.token' | tr -d \" || createdGithubToken=false)
-    if ${createdGithubToken} ]; then
+    GH_TOKEN=$(curl -X POST -H "Authorization: Bearer ${CLUSTER_SCAN_JWT}" -H "Accept: application/vnd.github.machine-man-preview+json" https://api.github.com/app/installations/"${GH_INSTALLATION_ID}"/access_tokens | jq '.token' | tr -d \" || true)
+    if [ ${GH_TOKEN} == "ghs*" ]; then
+      echo "Created GH_TOKEN"
+      return 0
+    else
       echo "Couldn't create GH_TOKEN"
       echo "${GH_TOKEN}"
       return 1
-    else
-      echo "Created GH_TOKEN"
-      return 0
     fi
 }
 
