a007d42f3e37d26709a39d4af5dc816b08857765
Timo Pagel <github@timo-pagel.de>
2022-08-21 11:12:22 +0200

    install sleep

--- test_actions/setup.bash
+++ test_actions/setup.bash
@@ -42,7 +42,7 @@ sed -i "s#SLACK_CLI_TOKEN_SECRET#${SLACK_CLI_TOKEN_SECRET}#" ${DEPLOYMENT_PATH}/
 sed -i "s#GITHUB_APP_ID_PLACEHOLDER#${GH_APP_ID}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github.env
 sed -i "s#GITHUB_APP_LOGIN_PLACEHOLDER#${GH_APP_LOGIN}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github.env
 sed -i "s#GITHUB_INSTALLATION_ID_PLACEHOLDER#${GH_INSTALLATION_ID}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github.env
-if [ -e ${GH_PRIVATE_KEY_PATH} ]; then
+if [ -f ${GH_PRIVATE_KEY_PATH} ]; then
   cp "${GH_PRIVATE_KEY_PATH}" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github_private_key.pem
 fi
 if [ "${GH_PRIVATE_KEY}" != "" ]; then
