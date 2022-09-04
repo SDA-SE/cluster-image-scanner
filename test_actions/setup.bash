c8cd8da0e58688450da98b9dcbf5c16a0934494a
Timo Pagel <github@timo-pagel.de>
2022-09-04 11:41:15 +0200

    enhance resources

--- test_actions/setup.bash
+++ test_actions/setup.bash
@@ -53,6 +53,7 @@ if [ "${GH_PRIVATE_KEY_BASE64}" != "" ]; then
   echo "Found GH_PRIVATE_KEY_BASE64, putting it"
   echo "${GH_PRIVATE_KEY_BASE64}" | base64 -d > ${DEPLOYMENT_PATH}/overlays/test-local/config-source/github_private_key.pem
   export GH_PRIVATE_KEY_PATH=$(realpath "${DEPLOYMENT_PATH}/overlays/test-local/config-source/github_private_key.pem")
+  echo "GH_PRIVATE_KEY_PATH: ${GH_PRIVATE_KEY_PATH}"
 fi
 
 sed -i "s#DEPSCAN_DB_DRIVER_PLACEHOLDER#${DEPSCAN_DB_DRIVER_PLACEHOLDER}#" ${DEPLOYMENT_PATH}/overlays/test-local/config-source/depcheck.env
