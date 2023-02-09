34ba10bd65ab3afd1a59ce5c590749c6065ec886
Timo Pagel <github@timo-pagel.de>
2023-02-09 19:52:02 +0100

    use current cataloger

--- images/scan/syft/module.bash
+++ images/scan/syft/module.bash
@@ -5,8 +5,12 @@ source /clusterscanner/scan-common.bash
 scan_result_pre
 
 echo "Starting Syft with parameter '$@'"
-/syft "$@"
+/syft "$@" -v
 
+if [ -f /clusterscanner/data/bom.json ]; then
+  echo  "/clusterscanner/data/bom.json doesn't exists"
+  exit 2
+fi
 scan_result_post
 
 exit  0
