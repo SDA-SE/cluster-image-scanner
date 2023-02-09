c4ba98f2c018edb47d0da41b4c5d77e40bd3b9d1
Timo Pagel <github@timo-pagel.de>
2023-02-09 19:40:03 +0100

    use current cataloger

--- images/scan/syft/module.bash
+++ images/scan/syft/module.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e
+set -xe
 source /clusterscanner/scan-common.bash
 scan_result_pre
 
-echo "Starting Syft with parameter $@"
+echo "Starting Syft with parameter '$@'"
 /syft "$@"
 
 scan_result_post
