1b68d411f1d51ccf23fe83e6b581887aef9045c5
corrupt <corrupt@users.noreply.github.com>
2023-02-17 15:25:38 +0100

    Update module.bash
    typo fix

--- images/scan/new-version/module.bash
+++ images/scan/new-version/module.bash
@@ -17,7 +17,7 @@ function testNewImageAndReport {
   infoText="Image has a new tag, at least ${imageToTest}"
   JSON_RESULT=$(echo "${JSON_RESULT}" | jq -Sc ". += {\"status\": \"completed\", \"finding\": true, \"infoText\": \"${infoText}\", \"newVersion\": \"${imageToTest}\"}")
   #cp /clusterscanner/new-version.csv "${ARTIFACTS_PATH}/new-version.csv"
-  cp /clusterscanner/new-version.csv "${ARTIFACTS_PATH}/new-version.json"
+  cp /clusterscanner/new-version.json "${ARTIFACTS_PATH}/new-version.json"
   #sed -i "s|###INFOTEXT###|${infoText}|g" "${ARTIFACTS_PATH}/new-version.csv"
   #sed -i "s/###TITLE###/Image Has a New Version/" "${ARTIFACTS_PATH}/new-version.csv"
   #sed -i "s/###SEVERITY###/Low/" "${ARTIFACTS_PATH}/new-version.csv"
