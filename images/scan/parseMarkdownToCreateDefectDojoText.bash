a1d75edca8f02852be857269cf82fed085dbf518
corrupt <corrupt@users.noreply.github.com>
2023-02-17 15:37:50 +0100

    Update parseMarkdownToCreateDefectDojoText.bash

--- images/scan/parseMarkdownToCreateDefectDojoText.bash
+++ images/scan/parseMarkdownToCreateDefectDojoText.bash
@@ -2,7 +2,7 @@
 
 SOURCE_FILE=$1 # md from doc
 TARGET=$2 # What to extract Response or Relevance
-TARGET_FILE=$3 # Target json
+TARGET_FILE=$3 # Target CSV
 TEMP_FILE="/tmp/tmp-file-to-create-markdown"
 heading="##"
 
@@ -21,4 +21,7 @@ sed -i.bak "s/# //g" ${TEMP_FILE}
 sed -i.bak "s/#/\n/g" ${TEMP_FILE}
 
 extract=$(jq -R -s -c '.' ${TEMP_FILE})
-$(jq --arg extract "${extract}" '.findings[].relevance = ($extract | fromjson)' < "${TARGET_FILE}") > "${TARGET_FILE}"
+echo $(jq --arg extract "${extract}" '.findings[].relevance = ($extract | fromjson)' < "${TARGET_FILE}") > "${TARGET_FILE}"
+
+desc=$(jq '.findings[].description | to_entries | map(.value) | join("\n")' distroless.json)
+echo $(jq --arg desc "${desc}" '.findings[].description = ($desc | fromjson)' < distroless.json) > distroless.json
