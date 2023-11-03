fd3dcb404978ac1e8a4f43d029db55d4d2d09e04
Roland Schilling <corrupt@users.noreply.github.com>
2023-11-03 11:39:44 +0100

    chore: small things

--- images/process/image-source-fetcher/module.bash
+++ images/process/image-source-fetcher/module.bash
@@ -9,6 +9,7 @@ set -ex
 #echo "[profile cluster-scan]" > ~/.aws/config
 #echo "${S3_ROLE_ARN}" >> ~/.aws/config
 #/usr/local/aws-cli/v2/2.13.29/bin/aws ${S3_PARAMETER} "${S3_BUCKET}"
+mkdir -p /clusterscanner/out/merged
 curl --location 'https://api.test.sda-se.io/v1/all-image-collector-reports' \
 --header "x-api-key: ${API_KEY}" \
 --header "x-api-signature: ${SIGNATURE}" > /clusterscanner/out/merged/merged.json
