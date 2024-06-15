554c7576be15d39e88b45e316893e5d935432714
Timo Pagel <github@timo-pagel.de>
2024-06-15 10:10:28 +0200

    use ub8

--- images/process/notifier/build.sh
+++ images/process/notifier/build.sh
@@ -29,7 +29,7 @@ cleanup() {
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 build_dir="${dir}/build"
 
-base_image="registry.access.redhat.com/ubi9/ubi" # minimal doesn't have useradd
+base_image="registry.access.redhat.com/ubi8/ubi" # minimal doesn't have useradd
 ctr_tools="$(buildah from --pull --quiet ${base_image})"
 
 base_image="quay.io/sdase/cluster-image-scanner-base:3"
@@ -41,16 +41,16 @@ dnf_opts=(
   "--installroot=/mnt"
   "--assumeyes"
   "--setopt=install_weak_deps=false"
-  "--releasever=9"
+  "--releasever=8"
   "--setopt=tsflags=nocontexts,nodocs"
   "--quiet"
 )
 #rm -Rf "${mnt}/etc/yum.repos.d" || true
-curl -o "${mnt}/epel.rpm" -s -L https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+curl -o "${mnt}/epel.rpm" -s -L https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 #buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- cp -a /etc/yum.repos.d/ /mnt/etc/
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf "${dnf_opts[@]}" install /mnt/epel.rpm
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf "${dnf_opts[@]}" update
-buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf install "${dnf_opts[@]}" msmtp s-nail dos2unix # s-nail enhances mailx, msmtp replaces sendmail
+buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf install "${dnf_opts[@]}" mailx dos2unix
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- ln -s "${mnt}/usr/bin/msmtp" /usr/sbin/sendmail
 rm -rf "${mnt}/var/{cache,log}/*" "${mnt}/tmp/*"
 
