93bd745e8ed600f80cd08a0210138eb11583fd56
Timo Pagel <github@timo-pagel.de>
2024-06-15 10:14:56 +0200

    use base image 2

--- images/process/notifier/build.sh
+++ images/process/notifier/build.sh
@@ -32,7 +32,7 @@ build_dir="${dir}/build"
 base_image="registry.access.redhat.com/ubi8/ubi" # minimal doesn't have useradd
 ctr_tools="$(buildah from --pull --quiet ${base_image})"
 
-base_image="quay.io/sdase/cluster-image-scanner-base:3"
+base_image="quay.io/sdase/cluster-image-scanner-base:2"
 ctr="$(buildah from --pull --quiet $base_image)"
 mnt="$(buildah mount "${ctr}")"
 
@@ -47,7 +47,7 @@ dnf_opts=(
 )
 #rm -Rf "${mnt}/etc/yum.repos.d" || true
 curl -o "${mnt}/epel.rpm" -s -L https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-#buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- cp -a /etc/yum.repos.d/ /mnt/etc/
+buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- cp -a /etc/yum.repos.d/ /mnt/etc/
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf "${dnf_opts[@]}" install /mnt/epel.rpm
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf "${dnf_opts[@]}" update
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf install "${dnf_opts[@]}" mailx dos2unix
