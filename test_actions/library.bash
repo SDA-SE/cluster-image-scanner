223b70c14e3095ad1d29a0dea003dd1f762272d5
Alex <ajendrosch@users.noreply.github.com>
2024-07-07 23:00:49 +0200

    Update library.bash
    fixing that a branch does not overwrite the master image

--- test_actions/library.bash
+++ test_actions/library.bash
@@ -73,8 +73,8 @@ wait_for_pods_completed () {
   done
 }
 
-#export BRANCH=$(git rev-parse --abbrev-ref HEAD)
-export BRANCH="master"
+export BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#export BRANCH="master"
 export MAJOR="3"
 export MINOR="0"
 export PATCH="${GITHUB_RUN_NUMBER}"
