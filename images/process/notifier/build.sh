2f9a7ffc529439bd434e472306443066a24afd33
Alex <ajendrosch@users.noreply.github.com>
2025-01-13 14:08:58 +0100

    Update build.sh
    changing notifier ubi to version 9

--- images/process/notifier/build.sh
+++ images/process/notifier/build.sh
@@ -29,10 +29,10 @@ cleanup() {
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 build_dir="${dir}/build"
 
-base_image="registry.access.redhat.com/ubi8/ubi" # minimal doesn't have useradd
+base_image="registry.access.redhat.com/ubi9/ubi" # minimal doesn't have useradd
 ctr_tools="$(buildah from --pull --quiet ${base_image})"
 
-base_image="quay.io/sdase/cluster-image-scanner-base:2"
+base_image="quay.io/sdase/cluster-image-scanner-base:3"
 ctr="$(buildah from --pull --quiet $base_image)"
 mnt="$(buildah mount "${ctr}")"
 
@@ -41,12 +41,12 @@ dnf_opts=(
   "--installroot=/mnt"
   "--assumeyes"
   "--setopt=install_weak_deps=false"
-  "--releasever=8"
+  "--releasever=9"
   "--setopt=tsflags=nocontexts,nodocs"
   "--quiet"
 )
 #rm -Rf "${mnt}/etc/yum.repos.d" || true
-curl -o "${mnt}/epel.rpm" -s -L https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+curl -o "${mnt}/epel.rpm" -s -L https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- cp -a /etc/yum.repos.d/ /mnt/etc/
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf "${dnf_opts[@]}" install /mnt/epel.rpm
 buildah run --volume "${mnt}":/mnt "${ctr_tools}" -- /usr/bin/dnf "${dnf_opts[@]}" update
