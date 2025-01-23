72b937c291ffc207cdae8d2bcb201e5cda115838
Alex <ajendrosch@users.noreply.github.com>
2025-01-23 15:38:30 +0100

    Chore: update Syft build.sh
    Upgrade Syft Baseimage to v0.105.1 (latest < 1.X)

--- images/scan/syft/build.sh
+++ images/scan/syft/build.sh
@@ -29,7 +29,7 @@ cleanup() {
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 build_dir="${dir}/build"
 
-base_image="anchore/syft:v0.102.0"
+base_image="anchore/syft:v0.105.1"
 #base_image="quay.io/sdase/cluster-image-scanner-scan-syft:cataloger"
 
 ctr="$( buildah from --pull --quiet "${base_image}")"
